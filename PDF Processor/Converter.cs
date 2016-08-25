33347d5b00269265687b937ca0b1d64b78745dd5
Konstantin Nikolov <knikolov.1t@gmail.com>
2016-08-25 06:34:23 +0300

    Add files via upload

---
+++ PDF Processor/Converter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Processor
+{
+    public class Converter
+    {
+        public static string Dir = string.Empty;
+        public static string DirDest = string.Empty;
+
+        public static void Convert()
+        {
+            // Get values from UI
+            MainWindow.UI.Dispatcher.Invoke((Action)(() =>
+            {
+                MainWindow.UI.LogBox.Text += "Starting pdf processor.\n\n";
+                MainWindow.UI.LogBox.Text += "Processing...";
+                Dir = MainWindow.UI.SourceF.Text;
+                DirDest = MainWindow.UI.DestF.Text;
+            }));
+
+            //Process all images in folder and subfolders
+            ProcessDirectory(Dir);
+
+            //Log status
+            MainWindow.UI.Dispatcher.Invoke((Action)(() =>
+            {
+                MainWindow.UI.LogBox.Text += "Finished.\n";
+            }));
+        }
+
+        public static void ProcessDirectory(string Dir)
+        {
+            string[] fileEntries = Directory.GetFiles(Dir);
+            foreach (string fileName in fileEntries)
+            {
+                if (Path.GetExtension(fileName) == ".pdf" || Path.GetExtension(fileName) == ".PDF")
+                {
+                    Pdf.ToImages(fileName);
+                }
+                
+            }
+            // Recursion for subdirectories
+            string[] subdirectoryEntries = Directory.GetDirectories(Dir);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                ProcessDirectory(subdirectory);
+            }
+        }
+    }
+}
---
+++ PDF Processor/PDF.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace PDF_Processor
+{
+    public class Pdf
+    {
+       public static void ToImages(string path)
+        {
+            using( var processor = new GhostscriptRasterizer())
+            {
+                processor.Open(path);
+                int PageCount = processor.PageCount - 1;
+                // Creates folder with the same name as the pdf file
+                string ImageFolder = Path.GetDirectoryName(path) + @"\" + Path.GetFileNameWithoutExtension(path);
+                // Folder and files with the same name will be overwritten
+                Directory.CreateDirectory(ImageFolder);
+                for (int i = 1; i <= PageCount; i++)
+                {
+                    using (Image img = processor.GetPage(300, 300, i))
+                    {
+                        string SaveDir = ImageFolder + @"\page " + i.ToString() + ".jpg";
+                        img.Save(SaveDir, ImageFormat.Jpeg);
+                    }
+                }
+            }          
+        }      
+    }
+}
